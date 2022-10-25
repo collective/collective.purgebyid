2ae35d5f50c836d0dd805d389f1889bc530ab3a3
mamico <mauro.amico@gmail.com>
2022-10-25 02:14:45 +0200

    classifier py3.10

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Development Status :: 5 - Production/Stable",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
